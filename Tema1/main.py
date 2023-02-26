fbdf7b22d50199f529607c712bf709143249ca85
Eduard975 <theblackasasin@yahoo.com>
2023-02-26 18:56:00 +0200

    semne de punctuatie eliminate

--- Tema1/main.py
+++ Tema1/main.py
@@ -6,10 +6,12 @@ inlocuitori = [
     ":", ";", "/",
     "(", ")", "'", '"']
 
+print("Textul fara punctuatie: ")
 for linie in fin:
     for caracter in inlocuitori:
         if caracter in linie:
             linie = linie.replace(caracter, "")
+
     print(linie, end="")
 
 fin.close()
