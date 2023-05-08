5838885d6837876ee49e868de02365e58bc59b5f
Eduard975 <theblackasasin@yahoo.com>
2023-05-08 22:07:00 +0300

    restul din problema 2

--- Tema10/Problema2/src/main/kotlin/Main.kt
+++ Tema10/Problema2/src/main/kotlin/Main.kt
@@ -1,31 +1,68 @@
 import kotlin.concurrent.thread
-
+import kotlinx.coroutines.*
 class Vector(numbers: IntArray) {
     var array = numbers;
     var isSorted = false;
+    var isMultiplied = false;
 }
 
-fun main(args: Array<String>) {
-    val v = Vector(intArrayOf(26, 77, 32, 51, -14, 15, 20, 11, 6, 120));
-    val alpha = 6;
-    println("Vector initial: " + v.array.contentToString());
+fun threads(v : Vector, alpha: Int){
+    println("Procesare cu Threads\nVector initial: " + v.array.contentToString());
 
-    thread(){
+    thread(name="Inmultire"){
         for((i, number) in v.array.withIndex()){
             v.array[i] = v.array[i] * alpha;
         }
+        v.isMultiplied = true;
     }
 
-    thread(){
+    thread(name="Sortare"){
+        while(!v.isMultiplied) {
+            Thread.sleep(1);
+        }
         v.array.sort()
         v.isSorted = true;
     }
 
-
-    thread(){
+    thread(name="Afisare"){
         while(!v.isSorted) {
             Thread.sleep(1);
         }
         println("Vectorul nou: " + v.array.contentToString());
     }
+}
+
+fun coroutines(v : Vector, alpha: Int) = runBlocking<Unit>{
+    println("Procesare cu Coroutines\nVector initial: " + v.array.contentToString());
+
+    launch{
+        for((i, number) in v.array.withIndex()){
+            v.array[i] = v.array[i] * alpha;
+        }
+        v.isMultiplied = true;
+    }
+
+    launch{
+        while(!v.isMultiplied) {
+            delay(1);
+        }
+        v.array.sort()
+        v.isSorted = true;
+    }
+
+    launch{
+        while(!v.isSorted) {
+            delay(1);
+        }
+        println("Vectorul nou: " + v.array.contentToString());
+    }
+}
+
+fun main(args: Array<String>) {
+    val v = Vector(intArrayOf(26, 77, 32, 51, -14, 15, 20, 11, 6, 120));
+    val v2 = Vector(intArrayOf(226, -37, 22, 15, -1, 150, 201, 111, 62, 10));
+    val alpha = 6;
+
+    threads(v, alpha);
+    coroutines(v2, alpha);
 }
