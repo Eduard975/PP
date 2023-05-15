146330cb57d33ec32c2fb9fa4f585d365de845c6
Eduard975 <theblackasasin@yahoo.com>
2023-05-16 00:00:11 +0300

    Problema1

---
+++ Tema11/Problema1/main.py
@@ -0,0 +1,35 @@
+import asyncio
+
+
+async def sum(name, queue):
+    n = await queue.get()
+
+    await asyncio.sleep(1)
+
+    result = (n*(n+1))/2
+
+    queue.task_done()
+
+    print(f'Task {name}: Pentru n = {n} suma este {result}')
+
+
+async def main():
+    queue = asyncio.Queue()
+
+    courtines = []
+
+    queue.put_nowait(6)
+    queue.put_nowait(678)
+    queue.put_nowait(9800)
+    queue.put_nowait(323)
+
+
+    await asyncio.gather(
+        sum("1", queue),
+        sum("2", queue),
+        sum("3", queue),
+        sum("4", queue),
+    )
+
+if __name__ == '__main__':
+    asyncio.run(main())
