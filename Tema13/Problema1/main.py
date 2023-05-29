ea94b48d914a95c0d3f87bc0e172bf929eee450e
Eduard975 <theblackasasin@yahoo.com>
2023-05-30 00:41:41 +0300

    inceputul

---
+++ Tema13/Problema1/main.py
@@ -0,0 +1,8 @@
+
+
+def cuvinte():
+    text = 'Aceasta propozitie are multe cuvinte care incep cu litere diferite'
+    
+
+if __name__ == "__main__":
+    cuvinte()
---
+++ Tema13/Problema2/main.py
@@ -0,0 +1,7 @@
+from functional import seq
+def numere():
+    numbers = seq(1, 21, 75, 39, 7, 2, 35, 3, 31, 7, 8)
+
+
+if __name__ == "__main__":
+    numere()
